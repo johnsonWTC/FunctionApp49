d6ef45466a0e648b536d33d5039409e94e712c44
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-29 21:52:32 +0200

    Functions

---
+++ StartUp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(FunctionApp47.Startup))]
+
+
+namespace FunctionApp47
+{
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddScoped<ICrud, Crud>();
+        }
+    }
+}
